f20611b753469f7930fb95fe3809bc7121a0115d
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-09-08 14:50:45 -0300

    Se corrige la etiqueta del navbar

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -14,7 +14,7 @@ export default function RootLayout ({ children }) {
   return (
     <html lang='es'>
       <body className={inter.className}>
-        <Navbar></Navbar>
+        <Navbar />
         {children}
       </body>
     </html>
