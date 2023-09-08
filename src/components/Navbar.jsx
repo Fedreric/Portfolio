7d87f2500456df04828203566bf7eb8e71b7a019
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-09-08 15:56:22 -0300

    Se le agrega el active a los links

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,7 @@
-import Link from "next/link";
+'use client'
 
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 export default function Navbar () {
   const Links = [
     {
@@ -23,15 +25,23 @@ export default function Navbar () {
       route: "/contact"
     }
   ];
+  const pathname = usePathname();
+
   return (
     <header className='bg-slate-900'>
       <nav className=''>
         <ul className='flex gap-4 justify-end me-4'>
-          {Links.map((link) => (
-            <li key={link.route} className='text-xl my-4 ease-in-out duration-300 hover:scale-[1.1]'>
-              <Link href={link.route}>{link.label}</Link>
-            </li>
-          ))}
+          {Links.map((link) => {
+            const isActive = pathname === link.route;
+            return (
+              <li
+                key={link.route}
+                className={isActive ? 'text-xl underline decoration-solid my-4 ease-in-out duration-300 hover:scale-[1.1]' : 'text-xl my-4 ease-in-out duration-300 hover:scale-[1.1]'}
+              >
+                <Link href={link.route}>{link.label}</Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
