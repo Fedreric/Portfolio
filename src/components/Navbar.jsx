6372dd3652f4df4749b20c9af2e2fdb7e08ffff8
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-09-09 13:26:21 -0300

    Se corrigen estilos del navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -28,15 +28,15 @@ export default function Navbar () {
   const pathname = usePathname();
 
   return (
-    <header className='bg-slate-900'>
-      <nav className=''>
+    <header className='bg-slate-900 py-4'>
+      <nav>
         <ul className='flex gap-4 justify-end me-4'>
           {Links.map((link) => {
             const isActive = pathname === link.route;
             return (
               <li
                 key={link.route}
-                className={isActive ? 'text-xl underline decoration-solid my-4 ease-in-out duration-300 hover:scale-[1.1]' : 'text-xl my-4 ease-in-out duration-300 hover:scale-[1.1]'}
+                className={isActive ? 'text-xl underline decoration-solid my-4 ease-in-out duration-300 hover:scale-[1.07]' : 'text-xl my-4 ease-in-out duration-300 hover:scale-[1.07]'}
               >
                 <Link href={link.route}>{link.label}</Link>
               </li>
