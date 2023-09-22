96d7cd15ad2b80c4478cf9800904715905fb2b04
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-09-22 15:50:01 -0300

    Se agrega el loader para las paginas

---
+++ src/app/loading.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function loading () {
+  return (
+    <main>
+      <div className='h-full flex justify-center items-center'>
+        <span className='loader' />
+      </div>
+    </main>
+  );
+}
