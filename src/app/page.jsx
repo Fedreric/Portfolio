720d1b46e759b4c60008d78c866cebe69f81af2e
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-09-22 15:49:35 -0300

    Se agregan detalles esteticos en el h2

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,10 +6,10 @@ import BtnRedSocial from '@/components/Btn-red-social'
 export default function Home () {
   return (
     <main>
-      <section className='flex justify-around items-center mt-5'>
+      <section className='flex justify-evenly items-center mt-5'>
         <article>
           <h1 className='text-7xl'>Federico Ledesma</h1>
-          <h2 className='text-4xl'>Fullstack developer</h2>
+          <span className='text-4xl mt-1 border-b-2 border-r-2 border-violet-600 rounded-sm pe-2 pb-1'>Fullstack developer</span>
           <article className='flex gap-4 mt-3'>
             <BtnRedSocial redSocial={linkedin} link='https://www.linkedin.com/in/federico-luciano-ledesma-0623a6220/' />
             <BtnRedSocial redSocial={github} link='https://github.com/Fedreric' />
@@ -19,7 +19,7 @@ export default function Home () {
           <img
             src='https://comicvine.gamespot.com/a/uploads/scale_super/11160/111605805/8687496-monkey_d__luffy__gear_5____updated_by_b_a_i_o_r_e_t_t_o_df3gezw-fullview.png'
             alt='Federico Luciano Ledesma'
-            className='pe-10'
+            className=''
           />
         </picture>
       </section>
